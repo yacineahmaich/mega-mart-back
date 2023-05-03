bf3c9e100e6c78233b765de128e7e5cf646b1cc4
Hatim Hatim <Hatimkd7192@gmail.com>
2023-05-03 14:14:51 +0100

    refactore AuthController and improve validation

--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
 
 class LoginRequest extends FormRequest
 {
@@ -23,8 +24,8 @@ class LoginRequest extends FormRequest
     {
         return [
             //
-            'email' => 'required|email',
-            'password' => 'required'
+            'email' => ['required', 'email'],
+            'password' => ['required']
         ];
     }
 }
