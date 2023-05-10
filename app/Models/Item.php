5de8dd15f022653fc0517720f83c0aa9958f04b3
Yacine Ahmaich <yacineahmaich@gmail.com>
2023-05-10 22:02:06 +0100

    add orders & items tables

---
+++ app/Models/Item.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Item extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'quantity',
+        'price',
+        'product_id',
+        'order_id_id',
+    ];
+
+    public function order() {
+        return $this->belongsTo(Order::class);
+    }
+}
