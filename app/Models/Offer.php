64b8082783de2317c24ff9cd8925fd41531b1a8b
Yacine Ahmaich <yacineahmaich@gmail.com>
2023-07-25 20:34:33 +0100

    🚀 schedule outdated offers deletion command

--- app/Models/Offer.php
+++ app/Models/Offer.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -27,5 +28,10 @@ class Offer extends Model
     public function image()
     {
         return $this->morphOne(Image::class, 'imageable');
+    }
+
+    public static function deleteOutdatedOffers()
+    {
+        return Offer::query()->where('end', '<=', Carbon::now())->delete();
     }
 }
